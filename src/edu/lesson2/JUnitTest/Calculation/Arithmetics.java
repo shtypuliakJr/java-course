bb1bc28b78ed5a6d76208f21bba64c96efbe391c
shtypuliak_r <shtypuliak.jr@gmail.com>
2021-06-26 21:50:14 +0300

    JUnit example from video.

---
+++ src/edu/lesson2/JUnitTest/Calculation/Arithmetics.java
@@ -0,0 +1,20 @@
+package edu.lesson2.JUnitTest.Calculation;
+
+public class Arithmetics {
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double deduct(double a, double b) {
+        return a - b;
+    }
+
+    public double mult(double a, double b) {
+        return a * b;
+    }
+
+    public double divide(double a, double b) {
+        if (b == 0) throw new ArithmeticException();
+        return a / b;
+    }
+}
---
+++ src/edu/lesson2/JUnitTest/Test/TestArithmetics.java
@@ -0,0 +1,82 @@
+package edu.lesson2.JUnitTest.Test;
+
+import edu.lesson2.JUnitTest.Calculation.Arithmetics;
+import org.junit.*;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.Timeout;
+
+import static org.junit.Assert.*;
+
+public class TestArithmetics {
+    private static Arithmetics a;
+
+    @Rule
+    public final ExpectedException exp = ExpectedException.none();
+
+    @Rule
+    public Timeout time = new Timeout(1000);
+
+    @BeforeClass
+    public static void runT() {
+        a = new Arithmetics();
+    }
+
+    /*
+     * @After
+     * @AfterClass
+     * */
+    @AfterClass
+    public static void runTAfter() {
+        a = null;
+    }
+
+    @Test
+    public void testAdd() {
+        double res = a.add(3, 7);
+        Assert.assertEquals(10.0, res, 0);
+        //if (res == 10) fail();
+        //Assert.assertNull(res);
+        //Assert.assertTrue("Not equals 10",res == 10);
+        //Assert.assertFalse(res == 10);
+    }
+
+    @Ignore
+    @Test
+    public void testDeduct() {
+        double res = a.deduct(3, 7);
+        Assert.assertEquals(-4.0, res, 0);
+    }
+
+    @Test
+    public void testMult() {
+        double res = a.mult(3, 7);
+        Assert.assertEquals(21.0, res, 0);
+    }
+
+    @Test
+    public void testDivide() {
+        double res = a.divide(10, 5);
+        Assert.assertEquals(2.0, res, 0);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDivException() {
+        a.divide(10.0, 0.0);
+    }
+
+    @Test
+    public void testRuleDivException() {
+        exp.expect(ArithmeticException.class);
+        a.divide(10.0, 0.0);
+    }
+
+    @Test(timeout = 1000)
+    public void testTime() {
+        a.divide(10.0, 10.0);
+    }
+
+    @Test
+    public void testRuleTime() {
+        while (true) ;
+    }
+}
