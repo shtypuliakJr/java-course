2ba4258342cd03f9fc98e4f89d7908d1381d75bf
shtypuliak_jr <shtypuliak.jr@gmail.com>
2021-07-09 18:32:25 +0300

    gitignore changed

--- task3.RegistrationProgram/src/main/java/controllers/exceptions/LocaleSyntaxException.java
+++
@@ -1,7 +0,0 @@
-package controllers.exceptions;
-
-public class LocaleSyntaxException extends Exception {
-    public LocaleSyntaxException(String message) {
-        super(message);
-    }
-}
--- task3.RegistrationProgram/src/main/java/models/exception/LoginExistException.java
+++
@@ -1,5 +0,0 @@
-package models.exception;
-
-public class LoginExistException extends Exception{
-
-}
