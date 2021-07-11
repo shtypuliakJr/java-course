b7af6e2747bc4c92b4fbe22fdcfcac99dd9d8b65
shtypuliak.jr <shtypuliak.jr@gmail.com>
2021-07-11 15:54:57 +0300

    added try-catch-finally examples

---
+++ src/main/java/edu/lesson7/exception/habr/part1/TryCatchFinally.java
@@ -0,0 +1,142 @@
+package edu.lesson7.exception.habr.part1;
+
+public class TryCatchFinally {
+    public static void main(String[] args) {
+        try {
+            System.err.print(" 0");
+            // nothing
+            System.err.print(" 1");
+        } catch (Error e) {
+            System.err.print(" 2");
+        } finally {
+            System.err.print(" 3");
+        }
+        System.err.print(" 4");
+    }
+}
+
+//----------------------------------------------
+
+class TryCatchFinally1 {
+    public static void main(String[] args) {
+        try {
+            System.err.print(" 0");
+            if (true) {
+                throw new Error();
+            }
+            System.err.print(" 1");
+        } catch (Error e) {
+            System.err.print(" 2");
+        } finally {
+            System.err.print(" 3");
+        }
+        System.err.print(" 4");
+    }
+}
+
+//----------------------------------------------
+
+class NestedTryCatchFinallyWithoutException {
+
+    public static void main(String[] args) {
+        try {
+            System.err.print(" 0");
+            try {
+                System.err.print(" 1");
+                // НИЧЕГО
+                System.err.print(" 2");
+            } catch (RuntimeException e) {
+                System.err.print(" 3"); // НЕ заходим - нет исключения
+            } finally {
+                System.err.print(" 4"); // заходим всегда
+            }
+            System.err.print(" 5");     // заходим - выполнение в норме
+        } catch (Exception e) {
+            System.err.print(" 6");     // НЕ заходим - нет исключения
+        } finally {
+            System.err.print(" 7");     // заходим всегда
+        }
+        System.err.print(" 8");         // заходим - выполнение в норме
+    }
+}
+
+//----------------------------------------------
+
+class NestedTryCatchFinallyWithNestedException {
+    public static void main(String[] args) {
+        try {
+            System.err.print(" 0");
+            try {
+                System.err.print(" 1");
+                if (true) {
+                    throw new RuntimeException();
+                }
+                System.err.print(" 2");
+            } catch (RuntimeException e) {
+                System.err.print(" 3"); // ЗАХОДИМ - есть исключение
+            } finally {
+                System.err.print(" 4"); // заходим всегда
+            }
+            System.err.print(" 5");     // заходим - выполнение УЖЕ в норме
+        } catch (Exception e) {
+            System.err.print(" 6");     // не заходим - нет исключения, УЖЕ перехвачено
+        } finally {
+            System.err.print(" 7");     // заходим всегда
+        }
+        System.err.print(" 8");         // заходим - выполнение УЖЕ в норме
+    }
+}
+
+//----------------------------------------------
+
+class NestedTryCatchFinallyWithExternalException {
+    public static void main(String[] args) {
+        try {
+            System.err.print(" 0");
+            try {
+                System.err.print(" 1");
+                if (true) {
+                    throw new Exception();
+                }
+                System.err.print(" 2");
+            } catch (RuntimeException e) {
+                System.err.print(" 3"); // НЕ заходим - есть исключение, но НЕПОДХОДЯЩЕГО ТИПА
+            } finally {
+                System.err.print(" 4"); // заходим всегда
+            }
+            System.err.print(" 5");     // не заходим - выполнение НЕ в норме
+        } catch (Exception e) {
+            System.err.print(" 6");     // ЗАХОДИМ - есть подходящее исключение
+        } finally {
+            System.err.print(" 7");     // заходим всегда
+        }
+        System.err.print(" 8");         // заходим - выполнение УЖЕ в норме
+    }
+}
+
+//----------------------------------------------
+
+class NestedTryCatchFinallyWithNoCatching {
+    public static void main(String[] args) {
+        try {
+            System.err.print(" 0");
+            try {
+                System.err.print(" 1");
+                if (true) {
+                    throw new Error();
+                }
+                System.err.print(" 2");
+            } catch (RuntimeException e) {
+                System.err.print(" 3"); // НЕ заходим - есть исключение, но НЕПОДХОДЯЩЕГО ТИПА
+            } finally {
+                System.err.print(" 4"); // заходим всегда
+            }
+            System.err.print(" 5");     // НЕ заходим - выполнение НЕ в норме
+        } catch (Exception e) {
+            System.err.print(" 6");     // не заходим - есть исключение, но НЕПОДХОДЯЩЕГО ТИПА
+        } finally {
+            System.err.print(" 7");     // заходим всегда
+        }
+        System.err.print(" 8");         // не заходим - выполнение НЕ в норме
+    }
+}
