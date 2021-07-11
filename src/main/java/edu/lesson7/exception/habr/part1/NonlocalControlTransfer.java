892d8fd31d8a142f201968c9ae790ce7d774751b
shtypuliak.jr <shtypuliak.jr@gmail.com>
2021-07-11 14:56:26 +0300

    examples of non-local control transfer

---
+++ src/main/java/edu/lesson7/exception/habr/part1/NonlocalControlTransfer.java
@@ -0,0 +1,95 @@
+package edu.lesson7.exception.habr.part1;
+
+public class NonlocalControlTransfer {
+    public static void main(String[] args) {
+        System.err.println("#1.in");
+        f(); // создаем фрейм, помещаем в стек, передаем в него управление
+        System.err.println("#1.out"); // вернулись
+    } // выходим из текущего фрейма, кончились инструкции
+
+    public static void f() {
+        System.err.println(".   #2.in");
+        g(); // создаем фрейм, помещаем в стек, передаем в него управление
+        System.err.println(".   #2.out");  //вернулись
+    } // выходим из текущего фрейма, кончились инструкции
+
+    public static void g() {
+        System.err.println(".   .   #3.in");
+        h(); // создаем фрейм, помещаем в стек, передаем в него управление
+        System.err.println(".   .   #3.out"); // вернулись
+    } // выходим из текущего фрейма, кончились инструкции
+
+    public static void h() {
+        System.err.println(".   .   .   #4.in");
+        if (true) {
+            System.err.println(".   .   .   #4.RETURN");
+            return; // выходим из текущего фрейма по 'return'
+        }
+        System.err.println(".   .   .   #4.out"); // ПРОПУСКАЕМ
+    }
+}
+//--------------------------------------------------------
+
+class NonlocalControlTransferThrowException {
+    public static void main(String[] args) {
+        System.err.println("#1.in");
+        f(); // создаем фрейм, помещаем в стек, передаем в него управление
+        System.err.println("#1.out"); // ПРОПУСТИЛИ!
+    }
+
+    public static void f() {
+        System.err.println(".   #2.in");
+        g(); // создаем фрейм, помещаем в стек, передаем в него управление
+        System.err.println(".   #2.out"); // ПРОПУСТИЛИ!
+    }
+
+    public static void g() {
+        System.err.println(".   .   #3.in");
+        h(); // создаем фрейм, помещаем в стек, передаем в него управление
+        System.err.println(".   .   #3.out"); // ПРОПУСТИЛИ!
+    }
+
+    public static void h() {
+        System.err.println(".   .   .   #4.in");
+        if (true) {
+            System.err.println(".   .   .   #4.THROW");
+            throw new Error(); // выходим со всей пачки фреймов ("раскрутка стека") по 'throw'
+        }
+        System.err.println(".   .   .   #4.out"); // ПРОПУСТИЛИ!
+    }
+}
+
+//--------------------------------
+
+class NonlocalControlTransferThrowExceptionAndCatch {
+    public static void main(String[] args) {
+        System.err.println("#1.in");
+        try {
+            f(); // создаем фрейм, помещаем в стек, передаем в него управление
+        } catch (Error e) { // "перехватили" "летящее" исключение
+            System.err.println("#1.CATCH");  // и работаем
+        }
+        System.err.println("#1.out");  // работаем дальше
+    }
+
+    public static void f() {
+        System.err.println(".   #2.in");
+        g(); // создаем фрейм, помещаем в стек, передаем в него управление
+        System.err.println(".   #2.out"); // ПРОПУСТИЛИ!
+    }
+
+    public static void g() {
+        System.err.println(".   .   #3.in");
+        h(); // создаем фрейм, помещаем в стек, передаем в него управление
+        System.err.println(".   .   #3.out"); // ПРОПУСТИЛИ!
+    }
+
+    public static void h() {
+        System.err.println(".   .   .   #4.in");
+        if (true) {
+            System.err.println(".   .   .   #4.THROW");
+            throw new Error(); // выходим со всей пачки фреймов ("раскрутка стека") по 'throw'
+        }
+        System.err.println(".   .   .   #4.out"); // ПРОПУСТИЛИ!
+    }
+}
