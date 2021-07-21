11590dfc5f659159f39fe7552de6eae2e0d24c1c
shtypuliakJr <shtypuliak.jr@gmail.com>
2021-07-21 17:12:09 +0300

    added new method to solve task1

--- src/main/java/edu/lesson11/FirstTask.java
+++ src/main/java/edu/lesson11/FirstTask.java
@@ -5,11 +5,23 @@ import java.util.*;
 public class FirstTask {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>(Arrays.asList(4, 5, -6, 4, 5, 3, 4, 2, 4, 5, 7));
-        Map<Integer, Integer> map = getUniqueElementsCounts(list);
-        printMap(map);
+
+        Map<Integer, Integer> mapImp = getUniqueElementsCountsImperative(list);
+
+        //printMap(mapImp);
+        mapImp.forEach((key, value) -> System.out.println(key + " - " + value));
     }
 
-    public static Map<Integer, Integer> getUniqueElementsCounts(List<Integer> list) {
+    public static Map<Integer, Integer> getUniqueElementsCountsUsingMapFunctions(List<Integer> list) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (Integer i : list) {
+            map.computeIfPresent(i, (k, y) -> y += 1);
+            map.putIfAbsent(i, 1);
+        }
+        return map;
+    }
+
+    public static Map<Integer, Integer> getUniqueElementsCountsImperative(List<Integer> list) {
         Map<Integer, Integer> map = new HashMap<>();
         for (Integer key : list) {
             Integer value = map.get(key);
