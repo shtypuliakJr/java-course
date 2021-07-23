a8df6b4467472f7bebc6a7b596919ddd80938a90
shtypuliakJr <shtypuliak.jr@gmail.com>
2021-07-23 03:47:18 +0300

    Solved task 2 Refactoring

--- src/main/java/edu/lesson13/Streams.java
+++ src/main/java/edu/lesson13/Streams.java
@@ -1,35 +1,40 @@
 package edu.lesson13;
 
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Streams {
     public static void main(String[] args) {
 
         // ToDo: Сделать массив int. Из него получить IntStream. Для него
-        int[] ints = {1, 20, 0, -12, 20, 0, 2, 100, 50, -16};
+        int[] ints = {1, 20, 0, -12, -20, 0, 2, 100, 50, -10};
         IntStream intStream = IntStream.of(ints);
 
         // ToDo: 1. Найти среднее значение элементов массива
-        System.out.print("Task 1: ");
-        intStream.average().ifPresent(System.out::println);
+        intStream.average().ifPresent(value -> System.out.println("Task 1: " + value));
 
         // ToDo: 2. Найти минимальный элемент, значение и индекс
-        //intStream.
+        IntStream.range(0, ints.length).boxed()
+                .reduce((i,j) -> ints[i] > ints[j] ? j : i)
+                .ifPresent(min -> {
+                    System.out.println("Task 2: ");
+                    System.out.println("\tMin element = " + ints[min]);
+                    System.out.println("\tIndex of min element = " + min);
+                });
 
         // ToDo: 3. Посчитать количество элементов равных нулю
-        System.out.print("Task 3: ");
         long countEqualsZero = Arrays.stream(ints).filter(v -> v == 0).summaryStatistics().getCount();
-        System.out.println(countEqualsZero);
+        System.out.println("Task 3: " + countEqualsZero);
 
         // ToDo: 4. Посчитать количество элементов больше нуля
-        System.out.print("Task 4: ");
         long countGreaterThanZero = Arrays.stream(ints).filter(v -> v > 0).count();
-        System.out.println(countGreaterThanZero);
+        System.out.println("Task 4: " + countGreaterThanZero);
 
         // ToDo: 5. Помножить элементы массива на число
         System.out.print("Task 5: ");
-        Arrays.stream(ints).map(v -> v * 3).forEach(v -> System.out.print(v + " "));
-
+        IntStream.of(ints).map(v -> v * 3).forEach(v -> System.out.print(v + " "));
     }
 }
